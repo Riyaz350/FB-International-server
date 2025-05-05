c8fe49e3a18c31639a195e61246e3f4d08957012
riyaz <riyaz3582@gmail.com>
2025-05-05 10:45:17 +0600

    updated index.js;

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-// src/index.js
 const app = require("./app");
 
 module.exports = app;
+
+// const app = require("./app")
+// const PORT = process.env.PORT || 5000;
+// require("dotenv").config();
+
+// const startApp = () => {
+//     app.listen(PORT, () => {
+//         console.log(`Running in ${PORT}`)
+//     })
+// }
+
+// startApp()
+
+
